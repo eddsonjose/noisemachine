602455b9f71b597f5db1d31322b588be1c521980
eddsonjose <44173160+eddsonjose@users.noreply.github.com>
2019-10-31 16:49:05 -0700

    update

---
+++ sketch.js
@@ -0,0 +1,12 @@
+//DOM
+//by Eddson Jose
+let sound;
+//-----------------------------------------------------------------------------
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(20);
+}
+//-----------------------------------------------------------------------------
+function draw() {
+  
+}
